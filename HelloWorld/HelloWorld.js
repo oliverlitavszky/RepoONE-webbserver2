c605cb2a08b67ca54110893a2bc66b46ae677550
OliverL <oliver.litavszky>
2020-09-04 13:18:54 +0200

    variabler

---
+++ HelloWorld/HelloWorld.js
@@ -0,0 +1,27 @@
+let OliverUser = "Oliver"
+let OliverAge = "18"
+
+
+if (OliverUser === "Oliver" && OliverAge == "18") {
+    console.log("Hey Oliver!")
+}
+else if (OliverUser != OliverUser && OliverAge <= OliverAge){
+    console.log("Not Oliver.")
+}
+else {
+    console.log("Who are you?")
+}
+
+let BobUser = "Bob"
+let BobAge = "60"
+
+if (BobUser === "Bob" && BobAge == "60") {
+    console.log("Hey Bob!")
+}
+
+else if (BobUser != BobUser && BobAge != BobAge){
+    console.log("Not Bob.")
+}
+else {
+    console.log ("Who are you stranger?")
+}
