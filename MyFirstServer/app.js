928a35b90e650aefbf71765a078c0ce657026e40
OliverL <oliver.litavszky>
2020-09-10 14:35:50 +0200

    post

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -3,8 +3,18 @@ const app = express()
 const port = 3000
 const clientDir = __dirname + '\\client\\'
 
+const bodyParser = require('body-parser')
+const { response } = require('express')
+
+app.use(bodyParser())
+
 app.get('/', (req, res) => res.sendFile(clientDir + 'index.html'))
 app.get('/kebab', (req, res) => res.sendFile(clientDir + 'style.css'))
 app.get('/bilder/weeho.jpg', (req, res) => res.sendFile(clientDir + '/bilder/weeho.jpg'))
+
+app.post('/', function(req, res) {
+    console.log(req.body)
+    res.redirect('/')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
